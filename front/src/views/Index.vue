176cd9f08c97d7e5d7987392085d902b03a1b8d6
xhsf <827032783@qq.com>
2021-10-27 21:57:45 +0800

    初始化

---
+++ front/src/views/Index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useStore } from 'vuex'
+import { useRoute, useRouter } from 'vue-router'
+const store = useStore()
+</script>
+
+<template>
+  <el-button  type="primary" @click="store.commit('increment')">增加1</el-button>
+  {{ store.state.count }}
+</template>
+
+<style scoped lang="less">
+</style>
